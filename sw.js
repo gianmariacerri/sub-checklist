f1fa6b18b3c10a6f1859cc7068b638329a718594
gianmariacerri <gianmaria.cerri@luxottica.com>
2026-09-10 17:54:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'sub-checklist-v2';
+const CACHE_NAME = 'sub-checklist-v5';
 const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await Promise.all(
+        FILES_TO_CACHE.map(async (url) => {
+          const response = await fetch(url, { cache: 'reload' });
+          return cache.put(url, response);
+        })
+      );
+    })
   );
   self.skipWaiting();
 });
@@ -12,9 +19,8 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
